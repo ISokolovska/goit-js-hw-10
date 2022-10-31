e009a15f259a4d9c3911b7a769af5e585a09b872
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-10-31 13:45:54 +0100

    дз 10

--- src/api/fetchCountries.js
+++ src/api/fetchCountries.js
@@ -1,14 +1,7 @@
-import { markupCountryInfo, markupCountryList } from '../index.js';
 import Notiflix from 'notiflix';
 
-export function fetchCountries(event) {
-  const countryInfo = document.querySelector('.country-info');
-  let name = event.target.value;
-  if (name === '') {
-    countryInfo.innerHTML = '';
-    return;
-  }
-  fetch(
+export function fetchCountries(name) {
+  return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   )
     .then(response => {
@@ -19,22 +12,8 @@ export function fetchCountries(event) {
       }
       throw new Error(response.statusText);
     })
-    .then(arrCountry => {
-      console.log(arrCountry);
-      if (arrCountry.length === 1) {
-        markupCountryInfo(arrCountry);
-      } else if (arrCountry.length >= 2 && arrCountry.length <= 10) {
-        markupCountryList(arrCountry);
-      } else if (arrCountry.length > 10) {
-        Notiflix.Notify.warning(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      } else {
-        console.log(arrCountry);
-        Notiflix.Notify.failure('Oops, there is no country with that name');
-      }
-    })
     .catch(error => {
       Notiflix.Notify.failure('Oops, there is no country with that name');
+      return error;
     });
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { fetchCountries } from './api/fetchCountries';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
-
+console.log(fetchCountries);
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-export const markupCountryList = (country = []) => {
+const markupCountryList = (country = []) => {
   const markup = country
     .map(({ name, flags }) => {
       return `<ul>
@@ -22,7 +22,7 @@ export const markupCountryList = (country = []) => {
   countryInfo.innerHTML = markup;
 };
 
-export const markupCountryInfo = (country = []) => {
+const markupCountryInfo = (country = []) => {
   const markup = country
     .map(({ name, capital, population, flags, languages }) => {
       const langString = Object.values(languages).join(', ');
@@ -38,5 +38,37 @@ export const markupCountryInfo = (country = []) => {
     .join('');
   countryInfo.innerHTML = markup;
 };
+function cleanSeach() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
 
-searchBox.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+function seachCountry(e) {
+  let name = e.target.value.trim();
+  if (name === '') {
+    cleanSeach();
+    return;
+  }
+  fetchCountries(name)
+    .then(arrCountry => {
+      cleanSeach();
+      console.log(arrCountry);
+      if (arrCountry.length > 10) {
+        Notiflix.Notify.warning(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      } else if (arrCountry.length >= 2 && arrCountry.length <= 10) {
+        markupCountryList(arrCountry);
+      } else if (arrCountry.length === 1) {
+        markupCountryInfo(arrCountry);
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      cleanSeach();
+      return error;
+    });
+}
+
+searchBox.addEventListener('input', debounce(seachCountry, DEBOUNCE_DELAY));
