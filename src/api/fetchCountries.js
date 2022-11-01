acba7c8fd4e209a79d1223089154691ed51c4e8f
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-11-01 14:28:56 +0100

    Del catch fetchCountries

--- src/api/fetchCountries.js
+++ src/api/fetchCountries.js
@@ -3,17 +3,12 @@ import Notiflix from 'notiflix';
 export function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => {
+  ).then(response => {
+    // console.log(response);
+    if (response.ok) {
       // console.log(response);
-      if (response.ok) {
-        // console.log(response);
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .catch(error => {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-      return error;
-    });
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  });
 }
